c3b0dcb19a193fb089d88f86bd089977c4338474
虞四明 <13966931523@163.com>
2018-09-06 21:49:03 +0800

    订单模块Service层,

--- bs/src/yu/bookstore/order/service/OrderService.java
+++ bs/src/yu/bookstore/order/service/OrderService.java
@@ -1,12 +1,44 @@
 package yu.bookstore.order.service;
 
+import cn.itcast.jdbc.JdbcUtils;
 import yu.bookstore.order.dao.OrderDao;
+import yu.bookstore.order.domain.Order;
+
+import java.sql.SQLException;
 
 /**
  * @Auther: yusiming
  * @Date: 2018/9/5 22:49
- * @Description:
+ * @Description: 业务层，
  */
 public class OrderService {
     private OrderDao orderDao = new OrderDao();
+
+    /**
+     * @Description: 添加顶单，需要处理事务，添加订单和添加订单条目是一个事务
+     * @auther: yusiming
+     * @date: 21:42 2018/9/6
+     * @param: []
+     * @return: void
+     */
+    public void add(Order order) {
+        try {
+            // 开启事务
+            JdbcUtils.beginTransaction();
+            // 添加订单
+            orderDao.addOrder(order);
+            // 添加订单条目
+            orderDao.addOrderItemList(order.getOrderItemList());
+            // 提交事务
+            JdbcUtils.commitTransaction();
+        } catch (SQLException e) {
+            try {
+                // 回滚事务
+                JdbcUtils.rollbackTransaction();
+            } catch (SQLException e1) {
+                throw new RuntimeException(e);
+            }
+            throw new RuntimeException(e);
+        }
+    }
 }
